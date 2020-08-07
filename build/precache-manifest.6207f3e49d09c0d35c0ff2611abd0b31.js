cb567b79384df06c11662bf5fa4f91b5eec2660e
ShubhamSagar <55043608+ssagar19@users.noreply.github.com>
2020-08-08 01:44:10 +0530

    Add files via upload

---
+++ build/precache-manifest.6207f3e49d09c0d35c0ff2611abd0b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2fed44500c35a9d2cfb505128109c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a1c595454cadb77451",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "0179406bde829c96fe8b",
+    "url": "/static/js/2.807bad6c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.807bad6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a1c595454cadb77451",
+    "url": "/static/js/main.36cac839.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
